f5b36b48181b862df21bfa264e189c7bd58f7341
Ken <outsideken@users.noreply.github.com>
2021-12-30 21:42:06 -0500

    Updated Code for Test
    Changed period from Month to Day

--- usgs.py
+++ usgs.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv'
+url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv'
 
 df = pd.read_csv(url)
 
