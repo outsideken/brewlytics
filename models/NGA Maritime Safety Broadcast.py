9326ad1b06e81ac5b64a9e4e83d4d185d20c8d6a
Ken <outsideken@users.noreply.github.com>
2022-05-12 20:08:15 -0400

    Update NGA Maritime Safety Broadcast.py

--- models/NGA Maritime Safety Broadcast.py
+++ models/NGA Maritime Safety Broadcast.py
@@ -1,9 +1,10 @@
-###########################################################################
+################################################################################
+################################################################################
 ## NGA Maritime Safety Broadcast
 ##
 ## Author: outsideKen
 ## Created: 11 December 2020
-## Updated: 04 May 2022
+## Updated: 07 May 2022
 ##
 ################################################################################
 ################################################################################
@@ -22,6 +23,10 @@
 ##              malformed reports to model owner for alerting purposes
 ##              - also update to use the new brewlytics Define Python Script
 ##                functional
+## 2022-05-07 - Added additional code to handle incomplete DTGs by imputing
+##              current year if missing from the In Force DTG; added additional
+##              handling if the In Force DTG regex pattern does not return
+##              an expected string
 ##
 ################################################################################
 ################################################################################
@@ -36,6 +41,18 @@ from datetime import datetime
 ################################################################################
 ## FUNCTIONS
 ################################################################################
+
+## Reports are not consistently separated with a separating sub-string; using 
+## the report DTG patten to separate joined reports
+def check_for_multiple_reports(rpt):
+    
+    global message_dtg_regex
+    
+    dtgs = re.findall(message_dtg_regex,rpt)
+    messages = [m.strip() for m in re.split(message_dtg_regex,rpt)
+                if m != '']
+
+    return ['%s%s' % tuple(z) for z in zip(dtgs,messages)]
 
 def get_latlons(report):
     
@@ -63,6 +80,19 @@ def convert2dd(ll):
         
     return dd
 
+def get_nav_id(rpt):
+    
+    global navarea_regex,hydro_regex
+    
+    if 'NAVAREA' in rpt:
+        nav_id = re.findall(navarea_regex,rpt)[0]
+    elif 'HYDRO' in rpt:
+        nav_id = re.findall(hydro_regex,rpt)[0]
+    else:
+        nav_id = '---'
+    
+    return nav_id
+
 def get_charts(report):
     
     global chart_pat1,chart_pat2
@@ -89,17 +119,56 @@ def get_country(report):
         
     return sorted(country)
 
+def extract_dtg(rpt):
+    
+    global dtg_patterns
+    
+    dtg = re.findall(dtg_patterns,rpt,flags=re.IGNORECASE)
+    if dtg:
+        
+        if (len(dtg[0]) == 11):
+            
+            dtg = '%s %s' % (dtg[0],now.strftime('%y'))
+            
+        elif (len(dtg[0]) == 14):
+            
+            dtg = dtg[0]
+            
+    ## If a valid/handled DTG format is not found return
+    ## return an Error Notification
+    else:
+        
+        dtg = pd.NaT
+    
+    return dtg
+
 def get_cancellation_date(report):
     
     global cancel_dtg_pat
 
     cancel_dtg = re.findall(cancel_dtg_pat,report,flags=re.IGNORECASE)
-    if cancel_dtg:        
-        cancelation_date = datetime.strptime(cancel_dtg[0],'%d%H%MZ %b %y')
-    else:
-        cancelation_date = None #'----'
+    
+    if not cancel_dtg:        
+        cancel_dtg = [None]
         
-    return cancelation_date
+    return cancel_dtg[0]
+
+def find_vessels(rpt):
+    
+    ## Find M/V names in reports based on M/V tag
+    vessels = list()
+    if ('M/V' in rpt) and ('DERELICT' not in rpt) and ('PIRATES' not in rpt):
+                      
+        for tag in [' TOWING',' ALONG',' AND',' IN ',',']:
+            mv = re.findall(r'M/V ([A-Z0-9\- ]*)' + tag,rpt)
+                
+            if mv:
+                vessels += ['M/V %s' % ship_name for ship_name in mv]
+            
+    if not vessels :
+        vessels = ['----']
+        
+    return vessels
 
 def extract_geometries(report):
     
@@ -186,6 +255,7 @@ def extract_geometries(report):
 ## MODEL DATA
 ################################################################################
 
+iso8601 = '%Y-%m-%dT%H:%M:%S.%fZ'
 now = datetime.utcnow()
 now_str = now.strftime('%Y-%m-%d %H%MZ')
 
@@ -193,13 +263,24 @@ now_str = now.strftime('%Y-%m-%d %H%MZ')
 ## REGEX Patterns
 ##------------------------------------------------------------------------------
 
-iso_format = '%Y-%m-%dT%H:%M:%S.%fZ'
-dtg_pat = r'[\d]{6}Z [A-Z]{3} [\d]{2}'
-cancel_dtg_pat = r'CANCEL THIS MSG ([\d]{6}Z [A-Z]{3} [\d]{2}).//'
+## Nav Id REGEX patterns
+navarea_regex = r'(NAVAREA[A-Z0-9\/\(\) ]*).'
+hydro_regex = r'(HYDRO[A-Z0-9\/\(\) ]*).'
 
+## In Force DTG REGEX patterns with and without the year
+dtg_patterns = r'[\d]{6}Z [A-Z]{3} [\d]{2}|[\d]{6}Z [A-Z]{3}'
+
+## Message DTG REGEX pattern
+message_dtg_regex = r'[\d]{6}Z [A-Z]{3} [\d]{2}\n'
+
+## Message Cancel DTG REGEX pattern
+cancel_dtg_pat = r'CANCEL THIS MSG ([\d]{6}Z [A-Z]{3} [\d]{2}).'
+
+## Chart REGEX patterns
 chart_pat1 = r'(CHART [\d]*)'
 chart_pat2 = r'(DNC [\d]{2})'
 
+## Latitude and Longitude REGEX patterns
 lat_pat = r'[\d]{1,2}-[\d\-.]{1,}[\d]{1}[NS]{1}'
 lon_pat = r'[\d]{1,3}-[\d\-.]{1,}[\d]{1}[WE]{1}'
 
@@ -222,7 +303,8 @@ countries = ['MEXICO','COLOMBIA','ECUADOR','EL SALVADOR','JAMAICA','GUYANA',
              'BAHRAIN','ISLA DE PASCUA','JAPAN','FIJI','MOZAMBIQUE','TAHITI',
              'IRAQ','SAUDI ARABIA','FRENCH POLYNESIA','NIUE','SAMOA','SUDAN',
              'SOMALIA','KENYA','TANZANIA','SOUTH AFRICA','COMOROS (MAYOTTE)',
-             'KIRIBATI','CRETE','MAURITANIA','MARSHALL ISLANDS','WALLIS AND FUTUNA',
+             'KIRIBATI','CRETE','MAURITANIA','MARSHALL ISLANDS',
+             'WALLIS AND FUTUNA',
              ]
 
 ##------------------------------------------------------------------------------
@@ -275,7 +357,9 @@ corrections = {'JOINING\n': 'JOINING ',
                'DUE TO ENGINE FAILURE IN 13-11N 110-56.': 'DUE TO ENGINE FAILURE IN 13-11N 110-56E.',
                'DART BUOY ADRIFT IN VICINITY 19-48S 172-00': 'DART BUOY ADRIFT IN VICINITY 19-48S 172-00W',
                '24-15.52E 052-00.53E': '24-15.52N 052-00.53E',
-               'M/V ASEAN PROTECTOR AN.': 'M/V ASEAN PROTECTOR AND'}
+               'M/V ASEAN PROTECTOR AN.': 'M/V ASEAN PROTECTOR AND',
+               '. \nCANCEL THIS MSG': '. CANCEL THIS MSG',
+               'RMKS/\nHYDROPAC 543/21(61).': 'RMKS/\n\nHYDROPAC 543/21(61).'}
 
 ################################################################################
 ## BODY
@@ -284,8 +368,12 @@ corrections = {'JOINING\n': 'JOINING ',
 malformed_reports = set()
 regions_found = set()
 output = list()
-for key,url in urls.items():
+for url_key,url in urls.items():
 
+    print()
+    print('Retrieving %s...' % url_key)
+    print()
+    
     ## Retrieve NGA Pacific/Atlantic Maritime Safety Broadcast
     r = requests.get(url)
 
@@ -296,12 +384,6 @@ for key,url in urls.items():
                if r not in ['']]
 
     ##--------------------------------------------------------------------------
-    ## Extract Broadcast DTG from the first report of the broadcast
-    dtg = re.findall(dtg_pat,reports[0],flags=re.IGNORECASE)
-    if dtg:
-        if_date = datetime.strptime(dtg[0],'%d%H%MZ %b %y')
-
-    ##--------------------------------------------------------------------------
     for report in reports[3:]:
 
         ## Scrub text for corrections to enable clean regex extraction
@@ -309,58 +391,45 @@ for key,url in urls.items():
             report = report.replace(key,val)
         
         ## Find M/V names in reports based on M/V tag
-        vessels = list()
-        if ('M/V' in report) and ('DERELICT' not in report) and ('PIRATES' not in report):
-                      
-            for tag in [' TOWING',' ALONG',' AND',' IN ',',']:
-                mv = re.findall(r'M/V ([A-Z0-9\- ]*)' + tag,report)
-                
-                if mv:
-                    vessels += ['M/V %s' % ship_name for ship_name in mv]
-            
-        if not vessels :
-            vessels = ['----']
-                
-        parsed = report[:-3].split('.\n')
+        vessels = find_vessels(report)
 
-        nav_id = parsed[0]
-
-        ## Extract Regions from report
+        ## Extract Non-Regional (Exception Reports) and Regions from reports
         for exception in ['(NAIS)','COVID','PANDEMIC','IRIDIUM','WARNINGS IN FORCE']:
+
+            parsed = report[:-3].split('.\n')
+            
             if exception in report:
                 
                 region = exception
                 
-                print('Exception Found: %s' % exception)
-                print()
+                print('- Exception Found: %s' % exception)
                 
                 break
                 
-            elif (len(parsed) > 2) and ('WARNINGS IN FORCE' not in parsed[1]):
-                
+            elif (len(parsed) > 2) and ('WARNINGS IN FORCE' not in report):
+
                 region = re.sub(r'\n',' ',parsed[1])
-                
+
                 ## Set of regions found in the data
                 regions_found.add(region)
-                
+
             else:
-                
+
                 ## Set malformed_report = True to send notification email
                 malformed_report_found = True
-                
+
                 malformed_reports.add(report)
                 
         ## Extract geometries from safety reports
         points,tracklines,polygons = extract_geometries(report)
                 
-        output.append({'NAV Area': nav_id,
-                       'Message DTG': dtg[0],
-                       'In Force Date': if_date,
-                       'Cancellation Date': get_cancellation_date(report),
+        output.append({'NAV Region': url_key,
+                       'NAV Area': get_nav_id(report),
+                       'Message DTG': extract_dtg(report),
+                       'Cancellation DTG': get_cancellation_date(report),
                        'Region': region,
                        'Country': ','.join(get_country(report)),
                        'Chart': ','.join(get_charts(report)),
-#                        'Narrative': '----',
                        'Raw Report': report.strip(),
                        'Vessels': '; '.join(vessels),
                        'Points': points,
@@ -372,28 +441,28 @@ odf = pd.DataFrame(output)
 
 if malformed_report_found:
     
-    print('Malformed NGA Maritime Safety Reports Encountered!')
+    print('Malformed/Unhandled NGA Maritime Safety Reports Encountered!')
     print('Preparing Notification email...')
     print()
     
-    subject = 'Malformed NGA Maritime Safety Reports Encountered'
+    subject = 'Malformed/Unhandled NGA Maritime Safety Reports Encountered'
     
     poc = 'This automated brewlytics Notification was generated on %s.' % now.strftime('%d %B %Y @ %H%MZ')
     
     stub = '<h2><b>%s</b></h2>' % subject
     stub += '<hr>'
     
-    for rep in malformed_reports:
+    for mal_rep in malformed_reports:
         
-        for r in rep.split('\n'):
-            stub += '''<p>%s</p>''' % r
+        for mr in mal_rep.split('\n'):
+            stub += '''<p>%s</p>''' % mr
         stub += '<hr>'
         
     stub += '<hr>'
     stub += '<p><center>%s</center></p>' % poc
     
-    email = {'To{string}': inputs.string,
-             'CC{string}': inputs.string,
+    email = {'To{string}': 'toedfish@yahoo.com',
+             'CC{string}': 'toedfish@yahoo.com',
              'Subject{string}': subject,
              'Body{string}': stub}
 
